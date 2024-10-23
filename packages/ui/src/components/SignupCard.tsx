93fa728dc175d4fb07ed76148716801da77b30d9
ashkkr <ashutoshsangra@gmail.com>
2024-10-23 12:55:28 +0530

    formatting using prettier, adds a prettier config file

--- packages/ui/src/components/SignupCard.tsx
+++ packages/ui/src/components/SignupCard.tsx
@@ -1,13 +1,28 @@
-import { Button, Card, TextField } from "@mui/material";
-import { useState } from "react";
+import { Button, Card, TextField } from '@mui/material';
+import { useState } from 'react';
 
-export function SignupCard(){
-    const [email,setEmail] = useState("");
-    const [pswd,setPswd] = useState("");
+export function SignupCard() {
+  const [email, setEmail] = useState('');
+  const [pswd, setPswd] = useState('');
 
-    return <Card>
-        <TextField label="email" onChange={(e) => setEmail(e.target.value)}></TextField>
-        <TextField label="password" onChange={(e) => setPswd(e.target.value)}></TextField>
-        <Button>Sign Up</Button>
+  return (
+    <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '30vw',
+        margin: 'auto',
+        marginTop: '20vh',
+        gap: '20px',
+        padding: '40px',
+      }}>
+      <TextField
+        label="email"
+        onChange={(e) => setEmail(e.target.value)}></TextField>
+      <TextField
+        label="password"
+        onChange={(e) => setPswd(e.target.value)}></TextField>
+      <Button>Sign Up</Button>
     </Card>
+  );
 }
